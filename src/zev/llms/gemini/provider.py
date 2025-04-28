984b128836c6640b1a6354df36de659da35894e8
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2025-04-28 08:04:02 +0600

    fix: update error handling and validation for Gemini API key in GeminiProvider

--- src/zev/llms/gemini/provider.py
+++ src/zev/llms/gemini/provider.py
@@ -1,21 +1,19 @@
-from openai import OpenAI
-
-from zev.llms.inference_provider_base import InferenceProvider
-from zev.llms.openai.provider import OpenAIProvider
-from zev.config import config
 from google import genai
-from zev.llms.types import OptionsResponse
-from zev.constants import PROMPT
 import json
 
+from zev.config import config
+from zev.constants import PROMPT, GEMINI_DEFAULT_MODEL
+from zev.llms.inference_provider_base import InferenceProvider
+from zev.llms.openai.provider import OpenAIProvider
+from zev.llms.types import OptionsResponse
 
 class GeminiProvider(InferenceProvider):
     def __init__(self):
-        if not config.gemini_model:
-            raise ValueError("GEMINI_MODEL must be set. Try running `zev --setup`.")
+        if not config.gemini_api_key:
+            raise ValueError("GEMINI_API_KEY must be set. Try running `zev --setup`.")
 
         self.client = genai.Client(api_key=config.gemini_api_key)
-        self.model = config.gemini_model
+        self.model = config.gemini_model or GEMINI_DEFAULT_MODEL
 
     def get_options(self, prompt: str, context: str) -> OptionsResponse | None:
         try:
@@ -28,15 +26,8 @@ class GeminiProvider(InferenceProvider):
                     'response_schema': OptionsResponse,
                 },
             )
-            try:
-                response_json = json.loads(response.text)
-                return OptionsResponse.parse_obj(response_json)
-
-            except json.JSONDecodeError as json_err:
-                print(f"Error: Failed to parse JSON response from Gemini API: {json_err}")
-                print(f"Raw response: {response.text[:100]}...") 
-                return None
-
-        except Exception as e:
-            print(f"Error: There was an error with your Gemini API key. You can change it by running `zev --setup`.") 
-            return
+            return response.parsed
+        except genai.errors.ClientError as e:  
+            print("Error:", e.details['error']['message'])  
+            print("Note that to update settings, you can run `zev --setup`.")  
+            return  
